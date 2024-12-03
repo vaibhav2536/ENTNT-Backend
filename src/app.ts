2162ce71b036defee28ca786410739f6b76b1aed
vaibhav dinkar <vaibhavdinkar2536@gmail.com>
2024-12-03 15:58:12 +0530

    changes made

--- src/app.ts
+++ src/app.ts
@@ -42,7 +42,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(multer().none());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173","https://entnt-frontend-beta.vercel.app"],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   }),
 );
